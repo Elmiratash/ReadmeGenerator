12c3f7857891444aa617af99b03cc8647f6f352d
Elmira Tashvighi <elmira.tashvighi@gmail.com>
2021-12-09 07:03:18 -0800

    add some functions on generatemarkdown file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,8 +12,33 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `# ${data.title}
+# zdescription
+${ data,Description}
 
+##Table of Contents
+[Installation](#installation)
+  [Usage](#usage)
+  [License](#liscense)
+  [Contribution](#contribution)
+  [Tests](#tests)
+  [Questions](#questions)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  The application is covered under the ${data.license} license.
+  ${badge(data.license)}
+  ## Contribution
+  ${data.contribution}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  **Github Username:** ${data.username}
+  **Github Profile:** www.github.com/${data.username}/
+  
+  **Email Address:** ${data.email}
 `;
 }
 
