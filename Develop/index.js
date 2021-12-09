00c5cecce7449859a3b1f400c74273d676bd440e
Elmira Tashvighi <elmira.tashvighi@gmail.com>
2021-12-09 06:46:57 -0800

    add questions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,64 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generareMarkdown = require("./utils/generateMarkdown")
+const fileNameName = "README.md"
+
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is the description of your project?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "How do you install your ptoject?",
+        name: "instalation"
+    },
+    {
+        type: "input",
+        message: "What is the usage information?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What are the contribution guidelines?",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "What are the test instructions?",
+        name: "test"
+    },
+    {
+        type: "list",
+        message: "Please select a type of license.",
+        name: "license"
+        choices: [
+            "MIT",
+            "Apache",
+            "ISC",
+            "GUN GPLv3"
+        ]
+    },
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
